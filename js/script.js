a3152c6011f22b680d612e612cb5f67dc1ce9739
Aidan Stritch <aidan.stritch@gmail.com>
2019-05-14 18:30:02 +0000

    added scripts for toggling on click, changing colors on mouseleave and mouseenter, and added boxes that change color when clicked

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
 $('p').click(function(){
-    $('p').slideToggle('slow');
+    $(this).slideToggle('slow');
+})
+
+
+$('button').mouseenter(function(){
+    $(this).removeClass('makeRed').addClass('makeBlue');
+})
+
+$('button').mouseleave(function(){
+    $(this).removeClass('makeBlue').addClass('makeRed');
+})
+
+
+
+$(document).ready(function(){
+    $(".box").on("click", function(){
+        var classNames = $(this).attr("class").split(" ");
+        $("." + classNames[1]).css("background-color", "red");
+    });
 })
